b96cfa19ba5e67c0512ef91ed7d89a757af9f8c3
Inez Carrillo <inezcarrillo@Inezs-MacBook-Pro.local>
2024-01-28 19:58:25 -0600

    updated inquirer questions and updated .gitignore

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const markdown = require('./utilities/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt ([
+    {
+        message: "What is the name of your project?",
+        title: "title",
+        type: "input",
+    },
+    {
+        message: "What is your name?",
+        name: "name",
+        type: "input",
+    },
+    {
+        message: "Enter a description of your project.",
+        name: "discription",
+        type: "input",
+    },
+    {
+        message: "Enter installation instructions.",
+        name: "discription",
+        type: "input",
+    },
+    {
+        message: "Enter usage information.",
+        name: "discription",
+        type: "input",
+    },
+    {
+        message: "Please choose what license you would like to use.",
+        name: "list",
+        choices: ["MIT", "GPL", "BSL", "Ms-PL"],
+        type: "input",
+    },
+    {
+        message: "What is your GitHub username?",
+        name: "username",
+        type: "input",
+    },
+    {
+        message: "What is your email?",
+        name: "email",
+        type: "input",
+    },
+]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
