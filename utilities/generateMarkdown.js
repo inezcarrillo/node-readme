4971b0f6f89d805fbbea1dbdedc665684bc16223
Inez Carrillo <inezcarrillo@Inezs-MacBook-Pro.local>
2024-05-04 17:28:56 -0500

    created the functions to generate readme

--- utilities/generateMarkdown.js
+++ utilities/generateMarkdown.js
@@ -23,8 +23,12 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  return `##License 
-  This project is covered under the ${license} license. Click the link above to learn more the license.`
+  if (license !== 'None') {
+    return `## License
+
+    This project is covered under the ${license} license. Click the link above to learn more the license.`;
+  }
+  return '';
 }
 
 // TODO: Create a function to generate markdown for README
@@ -32,14 +36,23 @@ function generateMarkdown(data) {
   return `# ${data.title}
   ${renderLicenseBadge(data.licenses)}
 
+  ##Description
+  ${data.description}
+
   ## Table of Contents
-  * [Description](#description)
+
   * [Installation](#installation)
+
   * [Usage](#usage)
+  ${renderLicenseLink(data.license)}
   * [Licenses](#license)
+
   * [Contributing](#contributing)
+
   * [Tests](#tests)
+
   * [Questions](#questions)
+  
   * [Credits](#credits)
 
   ## Description
