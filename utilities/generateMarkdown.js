966768e3624ebd331f4839ef7760bb2db75f093a
Inez Carrillo <inezcarrillo@Inezs-MacBook-Pro.local>
2024-05-04 17:29:32 -0500

    moved files

--- utilities/generateMarkdown.js
+++ utilities/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+
 const fs = require('fs');
 
 // If there is no license, return an empty string
@@ -6,7 +6,7 @@ function renderLicenseBadge(license) {
   return `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`
 };
 
-// TODO: Create a function that returns the license link
+
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license === MIT) {
@@ -20,7 +20,7 @@ function renderLicenseLink(license) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
+
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license !== 'None') {
@@ -31,7 +31,7 @@ function renderLicenseSection(license) {
   return '';
 }
 
-// TODO: Create a function to generate markdown for README
+
 function generateMarkdown(data) {
   return `# ${data.title}
   ${renderLicenseBadge(data.licenses)}
